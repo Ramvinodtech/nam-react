1f6208e09dfb337f51c27bf97d2119301ba8f527
Rama Vinod <ramvinod.tech@gmail.com>
2026-02-21 18:34:00 +0530

    created multiple elements using React.createElement

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
+
+{/* <div id="parent">
+    <div id="child1">
+        <h1 id="heading1">heading1</h1>
+        <h1 id="heading1">heading2</h1>
+    </div>
+</div> */}
+
+
 const heading = React.createElement(
-    'h1',
+    'div',
     {id : 'heading', 
         abc: 'highlight'  // this object is part of props
     }, 
-    'Hello World from React' // children part of props
+    React.createElement(
+        'div',
+        {id : 'child1', 
+            abc: 'highlight'  // this object is part of props
+        }, 
+        [React.createElement(
+            'h1',
+            {id : 'heading1', 
+                abc: 'highlight'  // this object is part of props
+            }, 
+            'heading1'),
+        React.createElement(
+                'h1',
+                {id : 'heading2', 
+                    abc: 'highlight'  // this object is part of props
+                }, 
+                'heading2')
+        ]
+    ) 
 );
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);
