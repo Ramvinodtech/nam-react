3f8bf18e80c7c956812bb231689444cc42bc667e
Rama Vinod <ramvinod.tech@gmail.com>
2026-02-26 11:09:30 +0530

    created first React functional component

--- App.js
+++ App.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 const jsxHeading = <h1 id="heading" className="jsx-heading">Namaste React vers 🚀</h1>
+
+// Functional Component
+const HeadingComponent = () => {
+    return <h1 id="heading" className="jsx-heading"> Namaste React Functional component</h1>;
+}
+
+const Title = () => {
+    
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+
+root.render(<HeadingComponent/>);
