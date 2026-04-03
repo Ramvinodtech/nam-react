99f8d89d91c362bff95b433d3672567f76a53c31
Rama Vinod <ramvinod.tech@gmail.com>
2026-04-03 14:08:00 +0530

    useState react hook used in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantData from "../utils/mockData";
+import { useState } from "react";
+
 
 const Body = () => {
+    const [listOfRestaurants, setlistOfRestaurants] = useState(restaurantData); // Hooks should be called inside the component function
     return (
         <div className="body">
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
+            <div className="filter">
+                <button className="res-btn" onClick={
+                    () => {
+                        const filteredData = restaurantData.filter(res => res.info.avgRating > 4.2);
+                        setlistOfRestaurants(filteredData);
+
+                    }
+                }>Top Rated Restaurants</button>
+            </div>
             <div className="res-container">
                 {
-                    restaurantData.map(restaurant =>  <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+                    listOfRestaurants.map(restaurant =>  <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
                 }
                
                 
