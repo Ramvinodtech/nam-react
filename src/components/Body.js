9b886ec097246710b3cf7f76435a5b8f653ce065
Rama Vinod <ramvinod.tech@gmail.com>
2026-04-08 14:09:57 +0530

    optional  chaining to safely access nested properties

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,10 +7,18 @@ const Body = () => {
     const [listOfRestaurants, setlistOfRestaurants] = useState(restaurantData); // Hooks should be called inside the component function
 
     useEffect(() => {
-        console.log('effect called after rendering');
+        // console.log('effect called after rendering');
+        fetchData();
     },[]);
 
-    console.log('body jsx renders');
+    const fetchData = async () => {
+        const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.38430&lng=78.45830&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const jsonData = await response.json();
+        console.log(jsonData)
+        setlistOfRestaurants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }   
+
+    // console.log('body jsx renders');
     return (
         <div className="body">
             {/* <div className="search">Search</div> */}
